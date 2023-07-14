ebf3478e9616204730acf23fd92edeec963992a4
Shpala <danila.shpala@yandex.ru>
2023-07-14 13:04:59 +0300

    +kuo

---
+++ kuo/Find the unique number.js
@@ -0,0 +1,23 @@
+// Find the unique number
+function findUniq(arr) {
+	const unique = [...new Set(arr)];
+	console.log(unique)
+	for (let j in unique) {
+	  let countDuble = 0
+	  for (let i in arr) {
+		if (unique[j] === arr[i]) {
+		  countDuble += 1
+		}
+		if (countDuble > 1) {
+		  const filteredNumbers = unique.filter((number) => number !== unique[j])
+		  console.log(filteredNumbers)
+		  return filteredNumbers[0]
+		}
+	  }
+	}
+  }
+
+  // Test
+  
+  let nums = [3, 10, 3, 3, 3]
+  findUniq(nums)
--- main.js
+++ main.js
@@ -1,78 +1,20 @@
-function VigenèreCipher(key, abc) {
-
-  // Создает ключ-строку
-  const getStrKey = (str) => {
-    let repeatCount = Math.ceil(str.length / key.length)
-    return key.repeat(repeatCount).slice(0, str.length)
-  }
-  
-  // Возврашает индекс буквы
-  const getCharNum = (value, abc) => abc.includes(value) ? abc.indexOf(value) : value
-  // Возврашает массив с индексами букв
-  const keyToNum = (str) => {
-    let res = str
-      .split('')
-      .map(char => getCharNum(char, abc))
-    return res
-  }
-
-  this.encode = function (str) {
-    const encoding = (str, key, abc) => {
-      let mergedStr = [...str]
-      for (let index in str) {
-        console.log(str[index])
-        console.log(key[index])
-        if (Number.isInteger(mergedStr[index])) {
-          let endNum = (mergedStr[index] + key[index]) % abc.length
-          mergedStr[index] = abc[endNum]
-
-        }
+function findUniq(arr) {
+  const unique = [...new Set(arr)];
+  console.log(unique)
+  for (let j in unique) {
+    let countDuble = 0
+    for (let i in arr) {
+      if (unique[j] === arr[i]) {
+        countDuble += 1
       }
-      console.log(mergedStr.join(''))
-      return mergedStr.join('')
-    }
-
-    return encoding(
-      keyToNum(str),
-      keyToNum(getStrKey(str)),
-      abc
-    )
-  };
-
-  this.decode = function (str) {
-    const decoding = (str, key, abc) => {
-      let mergedStr = [...str]
-      for (let index in str) {
-        console.log(str[index])
-        console.log(key[index])
-        if (Number.isInteger(mergedStr[index])) {
-          let endNum = (mergedStr[index] - key[index])
-          if (endNum < 0) {
-            endNum = abc.length + endNum
-            console.log(endNum)
-          }
-          console.log(endNum)
-          mergedStr[index] = abc[endNum]
-
-        }
+      if (countDuble > 1) {
+        const filteredNumbers = unique.filter((number) => number !== unique[j])
+        console.log(filteredNumbers)
+        return filteredNumbers[0]
       }
-      console.log(mergedStr.join(' '))
-      return mergedStr.join('')
     }
-
-    return decoding(
-      keyToNum(str),
-      keyToNum(getStrKey(str)),
-      abc
-    )
-  };
+  }
 }
 
-
-let abc = "abcdefghijklmnopqrstuvwxyz";
-let key = "password"
-// let key = "abc"
-const c = new VigenèreCipher(key, abc);
-
-c.encode('codewars') // rovwsoiv
-c.decode('laxxhsj') // waffles
+let nums = [3, 10, 3, 3, 3]
+findUniq(nums)
